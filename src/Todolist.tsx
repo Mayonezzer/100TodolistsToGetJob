60d34bfd5f50ee0b9340482370fbc59eb410cbd7
Mayonezzer <therullfetard@gmail.com>
2023-03-30 19:06:42 +0300

    0.5/100 fixes

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 import {FilterType} from "./App";
+import s from './Todolist.module.css';
 
 type PropsType = {
     mainTitle: string
@@ -19,43 +20,78 @@ export type TaskType= {
 
 export const Todolist = (props: PropsType) => {
     let [inputTitle, setInputTitle] = useState<string>('')
+    let [error, setError] = useState<string | null>('')
+    let [buttonName, setButtonName] = useState<FilterType>('All')
 
-    const allFilterHandler = () => {
+    const mappedTasks = props.tasksList.map( (t) => {
+        const removeTaskHandler = () => { // вынесли наверх
+            props.removeTask(t.taskId)
+        }
+        const changeCheckboxHandler = (e: ChangeEvent<HTMLInputElement>) => {
+            props.changeCheckbox(t.taskId, e.currentTarget.checked)
+        }
+        return (
+            <li key={t.taskId} className={t.isDone ? s.isDone : ''}>
+                <button onClick={removeTaskHandler}>X</button>
+                <input
+                    onChange={changeCheckboxHandler}
+                    type="checkbox"
+                    checked={t.isDone}
+                />
+                <span>{t.taskTitle}</span>
+            </li>
+        // <li key={t.taskId} className={t.isDone ? s.isDone : ''}>
+        //     <button onClick={() => removeTaskHandler(t.taskId)}>
+        //         X</button>
+        //     <input
+        //         onChange={(e) =>changeCheckboxHandler(t.taskId, e.currentTarget.checked)}
+        //         type="checkbox" checked={t.isDone}/>
+        //     <span>{t.taskTitle}</span>
+        // </li>        это если функции вынести из mapa еще выше, а map останется в вёрстке
+        )
+    } )
+
+    const setAllFilterHandler = () => {
         props.filterTask('All')
+        setButtonName('All')
     }
-    const activeFilterHandler = () => {
+    const setActiveFilterHandler = () => {
         props.filterTask('Active')
+        setButtonName('Active')
     }
-    const completedFilterHandler = () => {
+    const setCompletedFilterHandler = () => {
         props.filterTask('Completed')
+        setButtonName('Completed')
     }
 
     const addTaskHandler = () => {
-        if (inputTitle.trim() !== '')
-        props.addTask(inputTitle.trim())
+        if (inputTitle.trim() !== '') {
+            props.addTask(inputTitle.trim())
+        } else {
+            setError('впиши че-нить плз')
+        }
         setInputTitle('')
     }
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setError('')
         setInputTitle(e.currentTarget.value)
     }
 
     const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
+        if (e.key === 'Enter' && e.ctrlKey) {
             addTaskHandler()
         }
     }
 
+    const disabledHandler = inputTitle.trim() === '' || inputTitle.length > 15
 
-
-
-
-
-
-
-
-
-
+    // const changeCheckboxHandler = (tID: string, newwIsDone: boolean) => {
+    //     props.changeCheckbox(tID, newwIsDone)
+    // }
+    // const removeTaskHandler = (tID: string) => {
+    //     props.removeTask(tID)
+    // }           это если функции вынести из mapa еще выше, а map останется в вёрстке
 
 
     return (
@@ -66,32 +102,20 @@ export const Todolist = (props: PropsType) => {
                     value={inputTitle}
                     onChange={onChangeHandler}
                     onKeyDown={onKeyDownHandler}
+                    className={error ? s.error : ''}
                 />
-                <button onClick={addTaskHandler}>+</button>
+                <button onClick={addTaskHandler}
+                        disabled={disabledHandler}
+                >+</button>
             </div>
+            {error && <div className={s.errorMessage}>{error}</div>}
             <ul>
-                {props.tasksList.map( (t) => {
-                    const removeTaskHandler = () => {
-                        props.removeTask(t.taskId)
-                    }
-                    const changeCheckboxHandler = (e: ChangeEvent<HTMLInputElement>) => {
-                        props.changeCheckbox(t.taskId, e.currentTarget.checked)
-                    }
-                    return (
-                        <li key={t.taskId}>
-                            <button onClick={removeTaskHandler} >X</button>
-                            <input
-                                onChange={changeCheckboxHandler}
-                                type="checkbox" checked={t.isDone}/>
-                            <span>{t.taskTitle}</span>
-                        </li>
-                    )
-                } )}
+                {mappedTasks}
             </ul>
             <div>
-                <button onClick={allFilterHandler}>All</button>
-                <button onClick={activeFilterHandler}>Active</button>
-                <button onClick={completedFilterHandler}>Completed</button>
+                <button className={buttonName === 'All' ? s.activeFilter : ''} onClick={setAllFilterHandler}>All</button>
+                <button className={buttonName === 'Active' ? s.activeFilter : ''} onClick={setActiveFilterHandler}>Active</button>
+                <button className={buttonName === 'Completed' ? s.activeFilter : ''} onClick={setCompletedFilterHandler}>Completed</button>
             </div>
         </div>
     )
